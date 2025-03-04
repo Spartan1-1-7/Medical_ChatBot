61a139233465d0a49d86d589884e81cf57f61abd
Arindam Shukla <arindamshrish@gmail.com>
2025-03-04 11:13:49 +0530

    modified the chainlit ui intialization and remove the context attribute

--- model.py
+++ model.py
@@ -65,11 +65,11 @@ async def start():
     await msg.send()
     msg.content = 'Hi, Welcome to the medical bot. what is your query?'
     await msg.update()
-    cl.context.user_session.set['chain']= chain
+    cl.user_session.set('chain', chain)
 
 @cl.on_message
 async def main(message):
-    chain = cl.context.user_session.get('chain')
+    chain = cl.user_session.get('chain')
     cb = cl.AsyncLangchainCallbackHandler(stream_final_answer=True, answer_prefix_tokens=['FINAL', 'ANSWER'])
     cb.answer_reached = True
     res = await chain.acall(message, callbacks=[cb])
